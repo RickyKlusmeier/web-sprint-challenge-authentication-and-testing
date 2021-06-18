2fa4da8dd6e40aa9a130c2d31ce3eef9405ce015
Ricky Klusmeier <rickyklusmeier@gmail.com>
2021-06-18 12:01:45 -0600

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,7 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const server = require('./server')
+describe('sanity', () => {
+  test('if server exists', () => {
+    expect(server).toBeDefined()
+  })
 })
+
